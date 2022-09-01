90562d757138849f2218f18ba3c845e48887c9d1
Evgenii Silenko <evgen.silen@gmail.com>
2022-09-01 22:15:55 +0300

    HW9 task 1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# goit-js-hw-09
+# goit-js-hw-08
+HW8
---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const refs = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+let timerId;
+
+refs.startButton.addEventListener('click', () => {
+  refs.startButton.setAttribute('disabled', 'disabled');
+  refs.stopButton.removeAttribute('disabled', 'disabled');
+  timerId = setInterval(setColor, 1000);
+});
+
+refs.stopButton.addEventListener('click', () => {
+  refs.stopButton.setAttribute('disabled', 'disabled');
+  refs.startButton.removeAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+});
